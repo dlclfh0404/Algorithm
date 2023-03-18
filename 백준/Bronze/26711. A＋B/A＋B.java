ed435cf743dfd213fcdf720476a6dda81197f213
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 17:50:36 +0900

    [Bronze V] Title: A+B, Time: 320 ms, Memory: 19276 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        
+        System.out.print(A.add(B));
+    }
+}
