d0282515bc0418734de5fb4ba6673822208454bc
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 17:55:43 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6148 ms, Memory: 400492 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,13 @@
+import java.math.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger N = sc.nextBigInteger();
+        BigInteger M = sc.nextBigInteger();
+        
+        System.out.println(N.multiply(M));
+    }
+}
