b95f0908bad21054616c3294de6cec15cfe3e69b
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 18:10:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String N = sc.nextLine();
+		
+		System.out.println(N.length());
+	}
+}
