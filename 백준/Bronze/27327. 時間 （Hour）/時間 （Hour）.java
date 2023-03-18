761b2120437b2ad041c56c5460971b37661cf175
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 17:49:30 +0900

    [Bronze V] Title: 時間 (Hour), Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		System.out.print(N*24);
+		
+	}
+
+}
