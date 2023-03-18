5d01037b1c84ad3006e15f343b5008d069f27450
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 17:47:25 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 232 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.BigInteger;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger N = sc.nextBigInteger();
+        BigInteger M = sc.nextBigInteger();
+        
+        System.out.println(N.divide(M));
+        System.out.println(N.remainder(M));
+    }
+}
