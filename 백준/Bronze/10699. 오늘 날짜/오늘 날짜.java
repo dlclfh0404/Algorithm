8e44453f3635e966f6af72e4ae11ee12202b0091
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-18 18:05:21 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 16792 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.time.*;
+
+public class Main{
+    public static void main(String[] args){
+        LocalDate Date = LocalDate.now();
+        System.out.println(Date);
+    }
+}
