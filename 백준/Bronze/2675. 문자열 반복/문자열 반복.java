5fd5114f270c4d7d4a406fcc9f32fcfe97baffb8
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-23 14:45:22 +0900

    [Bronze II] Title: 문자열 반복, Time: 236 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int t = sc.nextInt();
+			String str = sc.next();
+			
+			for(int j = 0; j < str.length(); j++) {
+				for(int k = 0; k < t; k++) {
+					System.out.print(str.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
