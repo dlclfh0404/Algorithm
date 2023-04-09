eff4921565e6e8bf4cb7643b676b9e09eee3baca
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-09 11:17:49 +0900

    [D1] Title: 대각선 출력하기, Time: 108 ms, Memory: 18,584 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,12 @@
+class Solution {
+	public static void main(String[] args) {
+		/* 대각선 출력하기 
+		 #++++
+		 +#+++
+		 ++#++
+		 +++#+
+		 ++++#
+		 */
+		System.out.print("#++++\n+#+++\n++#++\n+++#+\n++++#");
+	}
+}
