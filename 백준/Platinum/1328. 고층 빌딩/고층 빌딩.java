36e840509e7f4ecd678ce934586e7ecbdb2358f4
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-21 00:17:05 +0900

    [Platinum V] Title: 고층 빌딩, Time: 168 ms, Memory: 24044 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static long[][][] dp;
+	static int div = 1000000007;
+	
+    public static void main(String[] args) throws IOException{
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(bf.readLine());
+    	
+    	int n = Integer.parseInt(st.nextToken());
+    	int l = Integer.parseInt(st.nextToken());
+    	int r = Integer.parseInt(st.nextToken());
+    	
+    	dp = new long[n+1][n+1][n+1];
+    	
+    	dp1(n,l,r);
+    	System.out.println(dp[n][l][r]);
+    }
+    static void dp1(int n, int l, int r) {
+    	dp[1][1][1] = 1;
+    	for(int i = 2; i < n+1; i++) {
+    		dp[i][i][1] = dp[i][1][i] = 1;
+    		for(int j = 1; j < l+1; j++) {
+    			for(int k = 1; k < r+1; k++) {
+    				dp[i][j][k] = (dp[i - 1][j][k - 1] + dp[i - 1][j - 1][k] + (dp[i - 1][j][k] * (i-2)))%div;
+    			}
+    		}
+    	}
+    }
+}
