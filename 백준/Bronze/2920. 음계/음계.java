61688ecc326d5d0d909229dc0c3b29ee05f32d9b
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-21 13:28:28 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr= new int[8]; //다장조 개수
+		String result = "";
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i = 0; i < arr.length - 1; i++) {
+			if(arr[i + 1] == arr[i] + 1) {
+				result = "ascending";
+			}
+			else if (arr[i + 1] == arr[i] - 1) {
+				result = "descending";
+			}
+			else {
+				result = "mixed";
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+}
