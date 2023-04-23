f0c42bd4aceed5feb8b5b391c0effe04af77c2ae
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-23 19:08:03 +0900

    [Bronze III] Title: 대지, Time: 408 ms, Memory: 31092 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bf.readLine());
+		int[] x = new int[n], y = new int[n]; 
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(x);
+		Arrays.sort(y);
+		
+		System.out.print((x[n - 1] - x[0]) * (y[n-1] - y[0]));
+	}
+}
