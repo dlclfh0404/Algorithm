442c9c9ba7dc295f289944c11cfecff9d1c99431
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-04-24 10:52:31 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		Long n = Long.parseLong(bf.readLine());
+		
+		System.out.println(n*(n-1)/2);
+		System.out.println('2');
+	}
+}
