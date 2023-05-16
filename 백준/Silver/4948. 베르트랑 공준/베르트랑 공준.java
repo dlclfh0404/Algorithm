4a2d2672d7f28e4375ed04e9df205709ed0cafc7
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-05-16 14:58:51 +0900

    [Silver II] Title: 베르트랑 공준, Time: 352 ms, Memory: 29776 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int n = sc.nextInt();
+			if(n==0) {
+				break;
+			}
+			boolean arr[] = new boolean[2*n+1];
+			arr[0] = true;
+			arr[1] = true;
+			
+			for(int i = 2; i <= Math.sqrt(2*n+1); i++) {
+				for(int j= i * i; j < 2 * n + 1; j+=i) {
+					arr[j] = true;
+				}
+			}
+			int cnt = 0;
+			for(int i = n + 1; i < 2 * n + 1; i++) {
+				if(arr[i] == false) {
+					cnt++;
+				}
+			}
+			System.out.println(cnt);
+		}
+	}
+}
