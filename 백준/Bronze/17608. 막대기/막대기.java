4c96f6b7bc58b3d90c88a8dc6c75a9dd73a835af
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-06-19 10:49:21 +0900

    [Bronze II] Title: 막대기, Time: 904 ms, Memory: 113560 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();  // 막대기의 개수
+        int[] sticks = new int[N];  // 막대기들의 높이를 저장할 배열
+
+        for (int i = 0; i < N; i++) {
+            sticks[i] = scanner.nextInt();
+        }
+
+        int count = 1;  // 보이는 막대기의 개수, 최소한 첫 번째 막대기는 보인다.
+        int maxHeight = sticks[N - 1];  // 현재까지 가장 큰 막대기의 높이
+
+        // 뒤에서부터 순회하면서 가장 큰 막대기부터 시작하여 보이는 막대기인지 체크
+        for (int i = N - 2; i >= 0; i--) {
+            if (sticks[i] > maxHeight) {
+                count++;
+                maxHeight = sticks[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
