aca8d79c2073e51f714575e3963f595a4a1b6684
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-07-18 08:51:48 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(factorial(N));
+    }
+
+    public static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+}
