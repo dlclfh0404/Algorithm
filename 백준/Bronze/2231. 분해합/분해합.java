2aee4ec612c73e3015a7dec9a95f4d46e26db287
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-09-12 13:45:51 +0900

    [Bronze II] Title: 분해합, Time: 228 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
-public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int result = 0;
-		
-		for(int i = 0; i < N; i++) {
-			int number = i;
-			int sum = 0; // 각 자릿수 합 변수
-			
-			while(number != 0) {
-				sum += number % 10; // 각 자릿수 더하기
-				number /= 10;
-			}
-			
-			// i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
-			if(sum + i == N) {
-				result = i;
-				break;
-			}
-		}
-		System.out.println(result);
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int result = 0;
+        
+        for (int i = 1; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            // 각 자릿수의 합을 계산
+            while (number > 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            // 분해합이 N과 같다면 결과에 값을 저장하고 반복을 종료
+            if (i + sum == N) {
+                result = i;
+                break;
+            }
+        }
+        
+        System.out.println(result);
+    }
 }
