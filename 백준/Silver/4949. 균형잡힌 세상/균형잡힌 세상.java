b9b6d578b79c4be7d7b2ca5890f888af205a9c00
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-11-10 11:41:49 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 280 ms, Memory: 19920 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,68 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		String s;
-		
-		while(true) {		
-			s = in.nextLine();
-			
-			if(s.equals(".")) {	// 종료 조건문 
-				break;
-			}		
-			System.out.println(solve(s));
-		}
-	
-	}
-	
-	public static String solve(String s) {
-		
-		Stack<Character> stack = new Stack<>();
-		
-		for(int i = 0; i < s.length(); i++) {
-			
-			char c = s.charAt(i);	// i 번째 문자 
-			
-			// 여는 괄호일 경우 스택에 push 
-			if(c == '(' || c == '[') {
-				stack.push(c);
-			}
-			
-			// 닫는 소괄호 일 경우 
-			else if(c == ')') {
-				
-				// 스택이 비어있거나 pop할 원소가 소괄호랑 매칭이 안되는 경우 
-				if(stack.empty() || stack.peek() != '(') {
-					return "no";
-				}
-				else {
-					stack.pop();
-				}
-			}
-			
-			else if(c == ']') {
-				
-				// 스택이 비어있거나 pop할 원소가 대괄호랑 매칭이 안되는 경우 
-				if(stack.empty() || stack.peek() != '[') {
-					return "no";
-				}
-				else {
-					stack.pop();
-				}
-			}
-			
-			// 그 외의 경우에는 불필요한 문자들이기에 skip한다. 
-		}
-		
-		if(stack.empty()) {
-			return "yes";
-		}
-		else {
-			return "no";
-		}
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String input = br.readLine();
+
+            // 종료 조건
+            if (input.equals(".")) {
+                break;
+            }
+
+            // 괄호의 짝이 올바르게 맞는지 확인
+            if (isBalanced(input)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+    private static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == ']') {
+                // 스택이 비어있거나 짝이 맞지 않으면 false
+                if (stack.isEmpty() || !isPair(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+
+        // 스택이 비어있어야 모든 괄호의 짝이 맞음
+        return stack.isEmpty();
+    }
+
+    private static boolean isPair(char open, char close) {
+        return (open == '(' && close == ')') || (open == '[' && close == ']');
+    }
 }
